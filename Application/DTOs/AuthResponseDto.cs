f9c1d9221a748e2609ef640a2f40b655fd726012
José Gabriel Morais <gabriel8azevedo@gmail.com>
2026-05-04 11:28:33 -0300

    feat: add AuthResponseDto

---
+++ Application/DTOs/AuthResponseDto.cs
@@ -0,0 +1,7 @@
+public class AuthResponseDto
+{
+    public required string Id { get; set; }
+    public required string Name { get; set; }
+    public required string Token { get; set; }
+    public required string Email { get; set; }
+}
